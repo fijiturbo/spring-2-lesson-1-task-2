89bde6e7d08ba87c3ba9b571675af8cde1003c9b
Fijiturbo <abror.abror.31@mail.ru>
2021-03-27 18:47:03 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/spring2lesson1task2/Spring2Lesson1Task2Application.java
@@ -0,0 +1,13 @@
+package uz.pdp.spring2lesson1task2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2Lesson1Task2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring2Lesson1Task2Application.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/spring2lesson1task2/Spring2Lesson1Task2ApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.spring2lesson1task2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring2Lesson1Task2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
